044b32d1366f10b11e05df6424019d356b780c69
Daniel Olkowski <olkowski.daniel@gmail.com>
2019-03-24 19:30:10 +0100

    Migrate to androidx

--- app/src/main/java/com/olkowskidaniel/favcast/MainActivity.java
+++ app/src/main/java/com/olkowskidaniel/favcast/MainActivity.java
@@ -1,6 +1,6 @@
 package com.olkowskidaniel.favcast;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
