4397f2e476ea3613d4eb1792de01b11efc1a3b9a
Daniel Olkowski <olkowski.daniel@gmail.com>
2019-03-25 21:53:29 +0100

    Navigation Drawer showing up

--- app/src/main/java/com/olkowskidaniel/favcast/view/base/BaseActivity.java
+++ app/src/main/java/com/olkowskidaniel/favcast/view/base/BaseActivity.java
@@ -1,16 +1,57 @@
 package com.olkowskidaniel.favcast.view.base;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
+
+import com.google.android.material.navigation.NavigationView;
 import com.olkowskidaniel.favcast.R;
 
 public class BaseActivity extends AppCompatActivity {
+
+    @BindView(R.id.baseActivity_drawerLayout)
+    DrawerLayout baseActivity_drawerLayout;
+    @BindView(R.id.baseActivity_navView)
+    NavigationView baseActivity_navView;
+    @BindView(R.id.baseActivity_toolbar)
+    Toolbar baseActivity_toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_base);
+        ButterKnife.bind(this);
+        setSupportActionBar(baseActivity_toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setHomeAsUpIndicator(R.drawable.ic_menu_button);
+
+        baseActivity_navView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                item.setChecked(true);
+                baseActivity_drawerLayout.closeDrawers();
+                return true;
+            }
+        });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                baseActivity_drawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/com/olkowskidaniel/favcast/view/main/MainActivity.java
+++ app/src/main/java/com/olkowskidaniel/favcast/view/main/MainActivity.java
@@ -1,15 +1,21 @@
 package com.olkowskidaniel.favcast.view.main;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.olkowskidaniel.favcast.R;
+import com.olkowskidaniel.favcast.view.base.BaseActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Intent baseIntent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        baseIntent = new Intent(MainActivity.this, BaseActivity.class);
+        startActivity(baseIntent);
     }
 }
